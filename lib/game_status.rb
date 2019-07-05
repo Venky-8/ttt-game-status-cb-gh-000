0ed547cf87e4184d9aca1dd3b61639d98e17500e
Venkatesh Mangnale <mangnalev.1999@gmail.com>
2019-07-05 15:37:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,30 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.find{|win|
+    position_taken?(board, win[0]) and
+    position_taken?(board, win[1]) and
+    position_taken?(board, win[2])
+  }
+  draw?(board)
+end
+
+def full?(board)
+  board.none? {|i| i == nil and i == " "}
+end
+
+def draw?(board)
+  !won?(board) and full?(board)
+end
